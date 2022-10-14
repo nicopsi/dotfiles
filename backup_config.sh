71df828fabb02f1bd3a45919ed1004ccfa9f215d
Nico <serious.zoeas@slmail.me>
2022-10-14 20:05:33 -0300

    Update files

--- backup_config.sh
+++ backup_config.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 cd ~/
-cp -v .Xresources .vimrc .profile .bash_aliases .bashrc ~/Documents/Configs
+cp -v .Xresources .vimrc .profile .bash_aliases .bashrc ~/dotfiles
+cp -vR .ssh ~/Private/Aplicaciones/Configs
 
 cd ~/.config
-cp -vR conky dunst i3 kitty ranger rofi picom.conf redshift.conf ~/Documents/Configs/.config
+cp -vR conky dunst i3 kitty ranger rofi picom.conf redshift.conf ~/dotfiles/.config
 
-cd ~/
+cd ~/dotfiles
+git add .
+git commit -m "Update files"
+git push origin main
 
 echo "###### backup to ~/Documents/Configs complete ######"
