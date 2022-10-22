dac7bb073b5b603423703f3f610984294f71d94b
Nico <serious.zoeas@slmail.me>
2022-10-22 17:24:01 -0300

    Update files

--- i3-setup.sh
+++ i3-setup.sh
@@ -2,8 +2,8 @@
 
 # Copy config files to proper places
 cp -v .Xresources .vimrc .bash_aliases .profile ~/
-cp -vR ~/dotfiles/.config/ ~/.config
-sudo cp etc_X11_xorg.conf.d/90-touchpad.conf /etc/X11/xorg.conf.d
+cp -vR ~/dotfiles/.config/ ~/
+sudo cp etc_X11_xorg.conf.d/90-touchpad.conf /etc/X11/xorg.conf.d/
 sudo echo 'QT_QPA_PLATFORMTHEME=qt5ct' >> /etc/environment
 sudo chmod u+s /usr/bin/light
 
