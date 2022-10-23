e4fceca433c19d5c7d09719296f8aedf654e30af
Nico <serious.zoeas@slmail.me>
2022-10-23 14:49:57 -0300

    Update files

--- backup_config.sh
+++ backup_config.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-echo "Copying config files"
-sleep 1
+echo "                          "
+echo "## Copying config files ##"
+echo "##########################"
+sleep 2
+
 cd ~/
 cp -v .Xresources .vimrc .profile .bash_aliases .bashrc ~/dotfiles
 cp -vR .ssh ~/Private/Aplicaciones/Configs
+sleep 2
 
 cd ~/.config
 cp -vR conky dunst i3 kitty ranger rofi picom.conf redshift.conf user-dirs.dirs ~/dotfiles/.config
 cp -vR gtk-3.0/bookmarks ~/dotfiles/.config
-sleep 1
+sleep 2
 
-echo "Push to github"
-sleep 1
+echo "                          "
+echo "## Push to github  ##"
+echo "#####################"
+sleep 2
 cd ~/dotfiles
 git add .
 git commit -m "Update files"
 git push origin main
-sleep 1
+sleep 2
 
+echo "                          "
 echo "###### backup complete ######"
