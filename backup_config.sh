bf9149766c9640a6dff277567adeefd4902267f5
nicopsi <66449635+nicopsi@users.noreply.github.com>
2023-08-29 13:24:22 -0300

    Add files via upload
    Signed-off-by: nicopsi <66449635+nicopsi@users.noreply.github.com>

--- backup_config.sh
+++ backup_config.sh
@@ -5,25 +5,25 @@ echo "##########################"
 sleep 2
 
 cd ~/
-cp -v .Xresources .vimrc .profile .bash_aliases .bashrc ~/dotfiles
+cp -v .Xresources .vimrc .profile .bash_aliases .bashrc ~/Private/dotfiles
 cp -vR .ssh ~/Private/Aplicaciones/Configs
 sleep 2
 
 cd ~/.config
-cp -vR conky dunst i3 kitty ranger rofi picom.conf redshift.conf user-dirs.dirs ~/dotfiles/.config
-cp -vR gtk-3.0/bookmarks ~/dotfiles/.config
+cp -vR conky dunst i3 kitty ranger rofi picom.conf redshift.conf user-dirs.dirs ~/Private/dotfiles/.config
+cp -vR gtk-3.0/bookmarks ~/Private/dotfiles/.config/
 sleep 2
 
-echo "                          "
-echo "## Push to github  ##"
-echo "#####################"
-sleep 2
-cd ~/dotfiles
-git add .
-git commit -m "Update files"
-git push origin main
-sleep 2
-
-echo "                          "
-echo "###### backup complete ######"
-sleep 2
+#echo "                          "
+#echo "## Push to github  ##"
+#echo "#####################"
+#sleep 2
+#cd ~/dotfiles
+#git add .
+#git commit -m "Update files"
+#git push origin main
+#sleep 2
+#
+#echo "                          "
+#echo "###### backup complete ######"
+#sleep 2
